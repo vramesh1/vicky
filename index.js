310a3c81abe0c2c854873493b6c300823694f7c5
Vignesh Ramesh <vig@vt.edu>
2017-05-16 23:01:07 -0400

    vicky initial commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/vicky');
 });
 
 app.listen(app.get('port'), function() {
